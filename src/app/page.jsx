89c54580abd7d746033438de340e635708196886
manthan-l-simformsolutions <manthan.l@simformsolutions.com>
2024-06-27 12:14:39 +0530

    add contact link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./home.module.css";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -11,15 +12,19 @@ const Home = () => {
           blanditiis adipisci minima reiciendis a autem assumenda dolore.
         </p>
         <div className={styles.buttons}>
-          <button className={styles.button}>Learn More</button>
-          <button className={styles.button}>Contact</button>
+          <Link href="/about">
+            <button className={styles.button}>Learn More</button>
+          </Link>
+          <Link href="/contact">
+            <button className={styles.button}>Contact</button>
+          </Link>
         </div>
         <div className={styles.brands}>
-          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
         </div>
       </div>
       <div className={styles.imgContainer}>
-        <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
       </div>
     </div>
   );
